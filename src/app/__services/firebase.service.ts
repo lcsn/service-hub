c8ba94093ddebd08d49545e65d09eccf283a0d20
lars-christian.simon <lars-christian.simon@ewe.de>
2018-12-07 16:10:48 +0100

    Upload progress is now visualized and a little more comprehensible.

--- src/app/__services/firebase.service.ts
+++ src/app/__services/firebase.service.ts
@@ -24,10 +24,25 @@ export class FirebaseService {
   }
 
   uploadFile(file: File) {
+    // let fileName = this.editFileName(file.name);
+    // file.name = fileName;
     const storageChildRef = firebase.storage().ref().child('images/' + file.name);
-    storageChildRef.put(file).then((snapshot) => {
-      console.log('File uploaded!');
-    });
+    const uploadTask = storageChildRef.put(file, { contentType: file.type });
+    return uploadTask;
+    // .then((snapshot) => {
+    //   console.log('File uploaded!');
+    // })
+    // .catch((error: any) => {
+    //   console.log(error);
+    // });
   }
+
+  // private editFileName(fileName: string): string {
+  //   let _fileName = (fileName.substring(0, fileName.lastIndexOf('.')) || fileName);
+  //   let ext = fileName.substring(fileName.lastIndexOf('.') + 1, fileName.length);
+  //   fileName += '.' + new Date().getTime() + '.';
+  //   fileName += ext;
+  //   return _fileName;
+  // }
 
 }
--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,24 +1,35 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatMenuModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatProgressBarModule
+} from '@angular/material';
 
 @NgModule({
-    imports: [
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-        MatCardModule,
-        MatMenuModule
-    ],
-    exports: [
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-        MatCardModule,
-        MatMenuModule
-    ]
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatProgressBarModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatProgressBarModule
+  ]
 })
 export class AngularMaterialModule { }
